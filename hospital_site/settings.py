6cb566379418ac3b9bc838fedc2f0986a9db8937
midhun98 <midhunskani@gmail.com>
2023-01-14 10:53:37 +0530

    Patient Enquiries

--- hospital_site/settings.py
+++ hospital_site/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['web-production-dd3e.up.railway.app','127.0.0.1']
 
