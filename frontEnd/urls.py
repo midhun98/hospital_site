d7f2f7b7b08e993f4b95b1949286a0d88690a990
midhun98 <midhunskani@gmail.com>
2023-01-18 23:21:59 +0530

    Admin dashboard

--- frontEnd/urls.py
+++ frontEnd/urls.py
@@ -5,4 +5,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("base/", views.base, name='base'),
     path("", TemplateView.as_view(template_name="index.html"), name='index'),
+    path("dashboard/", TemplateView.as_view(template_name="dashboard.html"), name='index'),
 ]
