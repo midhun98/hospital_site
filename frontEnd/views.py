989e2fad9369acf71ed6d229acd3eec560093089
midhun98 <midhunskani@gmail.com>
2023-01-22 01:02:23 +0530

    Error pages added

--- frontEnd/views.py
+++ frontEnd/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.shortcuts import render
+
+
+def handler404(request, exception):
+    return render(request, 'error_pages/404.html')
+
+
+def handler500(request):
+    return render(request, 'error_pages/500.html')
+
+
+def handler401(request):
+    return render(request, 'error_pages/401.html')
