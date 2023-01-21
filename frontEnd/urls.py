6fe593e9cc85cbcd197311adb9661992716b30f5
midhun98 <midhunskani@gmail.com>
2023-01-21 16:01:36 +0530

    Admin dashboard

--- frontEnd/urls.py
+++ frontEnd/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name='index'),
     path("dashboard/", TemplateView.as_view(template_name="dashboard.html"), name='dashboard'),
     path("enquiries/", TemplateView.as_view(template_name="enquires.html"), name='enquiries'),
-    path("base/", TemplateView.as_view(template_name="base.html"), name='base'),
 ]
