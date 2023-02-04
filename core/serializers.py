920f3c8348c3e76d79ef61cc723d8f69811c7fcf
midhun98 <midhunskani@gmail.com>
2023-02-04 19:37:31 +0530

    Created post method

--- core/serializers.py
+++ core/serializers.py
@@ -29,7 +29,7 @@ class ContactFormSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('first_name', 'last_name', 'email', 'username')
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -37,11 +37,11 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Profile
-        fields = ('user', 'mobile', 'gender', 'address')
+        fields = ('user', 'mobile', 'gender', 'address', 'role')
 
     def create(self, validated_data):
         user_data = validated_data.pop('user')
-        user = User.objects.create_user(username=user_data['username'], email=user_data['email'])
+        user = User.objects.create_user(**user_data)
         profile = Profile.objects.create(user=user, **validated_data)
         return profile
 
@@ -50,15 +50,14 @@ class LabResultSerializer(serializers.ModelSerializer):
     class Meta:
         model = LabResult
         fields = '__all__'
-        extra_kwargs = {
-            'patient_visit': {'allow_null': True}
-        }
+    patient_visit = serializers.PrimaryKeyRelatedField(queryset=PatientVisit.objects.all(), required=False)
 
 
 class EmergencyContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = EmergencyContact
         fields = '__all__'
+    patient = serializers.PrimaryKeyRelatedField(queryset=Patient.objects.all(), required=False)
 
 
 class PatientVisitSerializer(serializers.ModelSerializer):
@@ -66,10 +65,8 @@ class PatientVisitSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PatientVisit
-        fields = ['patient', 'admission_date', 'discharge_date', 'amount_paid', 'balance_amount_paid',
-                  'follow_up_appointments', 'reason_for_visit', 'diagnosis', 'treatment_notes',
-                  'insurance_provider', 'policy_number', 'lab_results']
-
+        fields = '__all__'
+    patient = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Patient.objects.all(), required=False)
 
     def create(self, validated_data):
         lab_results_data = validated_data.pop('lab_results', [])
@@ -89,13 +86,18 @@ class PatientSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
-        profile_data = validated_data.pop('profile')
-        emergency_contacts_data = validated_data.pop('emergency_contacts')
-        patient_visits_data = validated_data.pop('patient_visits')
-        profile = Profile.objects.create(**profile_data)
+        profile_data = validated_data.pop('profile', [])
+        emergency_contacts_data = validated_data.pop('emergency_contacts', [])
+        patient_visits_data = validated_data.pop('patient_visits', [])
+
+        profile = ProfileSerializer.create(ProfileSerializer(), validated_data=profile_data)
         patient = Patient.objects.create(profile=profile, **validated_data)
         for emergency_contact_data in emergency_contacts_data:
-            EmergencyContact.objects.create(patient=patient, **emergency_contact_data)
+            emergency_contact_data['patient'] = patient
+            EmergencyContact.objects.create(**emergency_contact_data)
         for patient_visit_data in patient_visits_data:
-            PatientVisit.objects.create(patient=patient, **patient_visit_data)
+            lab_results_data = patient_visit_data.pop('lab_results', [])
+            patient_visit = PatientVisit.objects.create(patient=patient, **patient_visit_data)
+            for lab_result_data in lab_results_data:
+                LabResult.objects.create(patient_visit=patient_visit, **lab_result_data)
         return patient
