1ca995ab824a4627d72cd8ea46f0e3b8be2ae367
midhun98 <midhunskani@gmail.com>
2023-02-05 10:29:11 +0530

    Front end refactoring

--- patients/urls.py
+++ patients/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('api/patients/', views.PatientAPIView.as_view(), name='patient-view'),
+    path("patient-list/", TemplateView.as_view(template_name="patients/patient_list.html"), name='patient-list'),
 ]
