a0c3810258383ef3c5ae7f6bb2640d7c49fdd708
midhun98 <midhunskani@gmail.com>
2023-07-21 11:26:06 +0530

    urls changes

--- hospital_site/urls.py
+++ hospital_site/urls.py
@@ -14,16 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('', include('frontEnd.urls')),
     path('', include('patients.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+              # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [ re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
 handler401 = 'frontEnd.views.handler401'
 handler404 = 'frontEnd.views.handler404'
 handler500 = 'frontEnd.views.handler500'
