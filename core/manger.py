e857ab0d9c7d00f43133cfb67875c1e8b39a7963
Midhun.S <midhunskani@gmail.com>
2023-08-07 17:41:22 +0530

    models and admin changes

--- core/manger.py
+++ core/manger.py
@@ -15,4 +15,9 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
 
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
         return self.create_user(phone_number, password, **extra_fields)
