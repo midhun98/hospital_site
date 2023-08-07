bb930bbadab9748b4cabd7c2ee0645f849f54e21
Midhun.S <midhunskani@gmail.com>
2023-08-07 19:05:55 +0530

    models and admin changes

--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('api/appointments/<int:pk>/', views.AppointmentModify.as_view(), name='appointment-modify'),
     path('api/message/', views.ContactMessage.as_view(), name='message-view'),
     path('api/login/', views.login_api, name='login-api'),
+    path('api/otplesslogin/', views.otplesslogin, name='otpless-login-api'),
     path('api/logout/', views.logout_api, name='logout-api'),
     path('api/get_current_user/', views.get_current_user, name='logout-api'),
     path('api/', include(router.urls)),
