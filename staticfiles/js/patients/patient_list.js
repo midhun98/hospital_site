3f493c90b0ece03c09c3a8ad085a5c1a295e1503
Midhun.S <midhunskani@gmail.com>
2023-08-16 18:56:41 +0530

    static files

--- staticfiles/js/patients/patient_list.js
+++ staticfiles/js/patients/patient_list.js
@@ -1,16 +1,41 @@
-const dataTable = CustomDataTable({
-    targetTable: '#all-patients-table',
-    targetName: 'Patients',
-    searchInput: '#search-input',
-    sortOrder: [[0, 'asc']], // Default sorting by the first column in ascending order
-    searchPlaceholderText: 'Search patients...',
-    apiURL: '/api/patients/',
-    tableColumns: [
-        { data: 'id', title: 'ID' },
-        { data: 'profile.first_name', title: 'First Name' },
-        { data: 'profile.last_name', title: 'Last Name' },
-        { data: 'profile.email', title: 'Email', defaultContent: '' },    ],
-    columnDefinitions: [],
+$(document).ready(function () {
+    // Initialize DataTable
+    let patientTable = $('#patient-table').DataTable({
+        "processing": true,
+        "serverSide": true,
+        "searching": false, // Hide the search bar
+        "ajax": {
+            "url": "/api/patients/",
+            "data": function (data) {
+                let customData = {
+                    page: (data.start / data.length) + 1,
+                    page_size: data.length,
+                    ordering: data.order[0].dir, // Add ordering if needed
+                    phone_number: $('#search-input-phone').val(),
+                    inpatient_number: $('#search-input-inpatient').val(),
+                    outpatient_number: $('#search-input-outpatient').val()
+                };
+                return customData;
+            },
+            "dataSrc": function (json) {
+                json.recordsTotal = json.count;
+                json.recordsFiltered = json.count;
+                return json.results;
+            } // Specify the key in the API response containing the data array
+        },
+        "columns": [
+            {"data": "id", title: 'ID', defaultContent: ''},
+            {"data": "profile.first_name", title: 'First Name', defaultContent: ''},
+            {"data": "profile.last_name", title: 'Last Name', defaultContent: ''},
+            {"data": "profile.phone_number", title: 'Phone', defaultContent: ''},
+            {"data": "profile.email", title: 'Email', defaultContent: ''},
+            {"data": "inpatient_number", title: 'IP Number', defaultContent: ''},
+            {"data": "outpatient_number", title: 'OP Number', defaultContent: ''},
+        ]
+    });
+
+    // Handle search input changes
+    $('#search-input-phone, #search-input-inpatient, #search-input-outpatient').on('keyup', function () {
+        patientTable.ajax.reload();
+    });
 });
-
-
