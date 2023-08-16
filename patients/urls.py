ba79f02d9292aad32adb518b59aab9e182238883
Midhun.S <midhunskani@gmail.com>
2023-08-16 14:44:00 +0530

    Added the functionality to view and update patients. Made changes in ui and api

--- patients/urls.py
+++ patients/urls.py
@@ -8,6 +8,8 @@ router.register('patients', views.PatientViewSet)
 urlpatterns = [
     path("patient-list/", TemplateView.as_view(template_name="patients/patient_list.html"), name='list-patients'),
     path("patient-create/", TemplateView.as_view(template_name="patients/patient_create.html"), name='patient-create'),
+    path("patient-detail/", TemplateView.as_view(template_name="patients/patient_create.html"), name='patient-detail'),
+    path("patient/<int:patient_id>/", TemplateView.as_view(template_name="patients/patient_detail.html"), name='patient-detail'),  # Add this line
     path('api/', include(router.urls)),
 
 ]
