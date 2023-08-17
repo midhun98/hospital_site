523ed85b93278772f79332e3ce7e8226737707c3
Midhun.S <midhunskani@gmail.com>
2023-08-17 10:45:19 +0530

    static files changes

--- staticfiles/js/patients/patient_list.js
+++ staticfiles/js/patients/patient_list.js
@@ -31,11 +31,28 @@ $(document).ready(function () {
             {"data": "profile.email", title: 'Email', defaultContent: ''},
             {"data": "inpatient_number", title: 'IP Number', defaultContent: ''},
             {"data": "outpatient_number", title: 'OP Number', defaultContent: ''},
-        ]
+            {
+                "data": null,
+                "title": "Actions",
+                "defaultContent": '<button class="btn btn-primary btn-sm btn-view">Detail View/Edit</button>'
+            },
+        ],
     });
 
     // Handle search input changes
     $('#search-input-phone, #search-input-inpatient, #search-input-outpatient').on('keyup', function () {
         patientTable.ajax.reload();
     });
+
+    $('#patient-table').on('click', '.btn-view', function () {
+        var rowIndex = $(this).closest('tr').index();
+        var rowData = patientTable.row(rowIndex).data();
+
+        if (rowData) {
+            var patientId = rowData.id;
+            // Redirect to the detailed view/edit page for the patient
+            window.location.href = '/patient/' + patientId + '/';
+        }
+    });
+
 });
