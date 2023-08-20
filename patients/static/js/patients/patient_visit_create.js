711eda58483f0ec5b6db2dc1a3211704de08b887
Midhun.S <midhunskani@gmail.com>
2023-08-20 12:41:56 +0530

    Completed the views for adding patient visit.changes to ui and api

--- patients/static/js/patients/patient_visit_create.js
+++ patients/static/js/patients/patient_visit_create.js
@@ -9,13 +9,16 @@ $(document).ready(function () {
         let formData = {
             admission_date: $("#admission_date").val(),
             discharge_date: $("#discharge_date").val(),
+            visit_date: $("#visit_date").val(),
+            follow_up_appointments: $("#follow_up_appointments").val(),
             reason_for_visit: $("#reason_for_visit").val(),
             diagnosis: $("#diagnosis").val(),
+            treatment_notes: $("#treatment_notes").val(),
         };
 
         $.ajax({
             type: "POST",
-            url: "http://127.0.0.1:8000/api/patients/32/patient-visits/",
+            url: `/api/patients/${patientId}/patient-visits/`,
             data: JSON.stringify(formData),
             headers: {
                 'Content-type': 'application/json',
@@ -30,6 +33,7 @@ $(document).ready(function () {
                 });
             },
             error: function (xhr) {
+                console.error(xhr.responseJSON);  // Print the detailed error response
                 if (xhr.status === 400) {
                     displayFieldErrors(xhr.responseJSON);
                 } else {
@@ -39,3 +43,18 @@ $(document).ready(function () {
         });
     });
 });
+function displayFieldErrors(errors) {
+    for (let field in errors) {
+        if (errors.hasOwnProperty(field)) {
+            let inputElement = $("#" + field);
+            let errorMessage = errors[field][0];
+            displayError(inputElement, errorMessage);
+        }
+    }
+}
+
+function displayError(inputElement, errorMessage) {
+    let errorElement = $("<div class='error-message'></div>").text(errorMessage);
+    inputElement.addClass("input-error");
+    inputElement.after(errorElement);
+}
