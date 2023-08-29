13608a51886156dbf3330cc83480bfc8353fb99e
Midhun <midhunskani@gmail.com>
2023-08-29 10:26:04 +0530

    New app invoices. Created the models

---
+++ invoices/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+from invoices import models
+
+admin.site.register(models.Invoice)
+
+class InvoiceItemAdmin(admin.ModelAdmin):
+    list_display = ('invoice', 'description', 'quantity', 'unit_price', 'total_price')
+    readonly_fields = ('total_price',)
+
+admin.site.register(models.InvoiceItem, InvoiceItemAdmin)
