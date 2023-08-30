1e883be335b02a1cfbfa218b8956c6c2ad1570a7
Midhun <midhunskani@gmail.com>
2023-08-30 17:55:13 +0530

    model changes

--- patients/admin.py
+++ patients/admin.py
@@ -5,7 +5,19 @@ from django.contrib.auth.models import Group
 # Register your models here.
 from patients import models
 
-admin.site.register(models.Patient)
+
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'existence_status', 'dob', 'display_age')
+    readonly_fields = ('display_age',)
+
+    def display_age(self, obj):
+        age = obj.calculate_age() if obj.dob else None
+        return age
+
+    display_age.short_description = 'Age'  # This sets the column header in the admin
+
+
+admin.site.register(models.Patient, PatientAdmin)
 admin.site.register(models.EmergencyContact)
 admin.site.register(models.PatientVisit)
 admin.site.register(models.LabResult)
