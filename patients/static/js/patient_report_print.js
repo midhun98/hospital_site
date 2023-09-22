fd85259a4ba7975050d30250ed356f32612208b6
midhun98 <midhunskani@gmail.com>
2023-09-22 17:57:14 +0530

    Change the ck editor to read only in the print page

--- patients/static/js/patient_report_print.js
+++ patients/static/js/patient_report_print.js
@@ -32,14 +32,18 @@ $(document).ready(function () {
             // Assuming findings is the table data
             $('#tableContainer').html(data.findings);  // Inject table data into a container
 
-            ClassicEditor
-                .create( document.querySelector( '#tableContainer' ), {
-                    toolbar: [],  // Set an empty array to hide the toolbar
-                    readOnly: true  // Set the readOnly option to true to make CKEditor read-only
-                })
-                .catch( error => {
-                    console.error( error );
-                });
+ClassicEditor
+    .create( document.querySelector( '#tableContainer' ), {
+        toolbar: [],  // Set an empty array to hide the toolbar
+    })
+    .then( editor => {
+        editor.enableReadOnlyMode('lock1'); // Enable read-only mode
+
+        // Do something here...
+    })
+    .catch( error => {
+        console.error( error );
+    });
 
 
             // Update the invoice name and report summary
