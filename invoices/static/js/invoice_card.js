13a91fa111f9d1a9b939f52ba230489a6cf5c34d
midhun98 <midhunskani@gmail.com>
2023-09-27 18:17:35 +0530

    Minor updates

--- invoices/static/js/invoice_card.js
+++ invoices/static/js/invoice_card.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
             // Populate the table cells with data from the API response
             $("#patient_name").text(data.profile.first_name + " " + data.profile.last_name);
             $("#patient_address").text(data.address);
-            $("#patient_email").text(data.profile.email);
+            $("#patient_email").text(data.profile.email === null ? '' : data.profile.email);
             $("#patient_phone").text(data.profile.phone_number);
             fileName = data.profile.first_name + " " + data.profile.last_name
         },
