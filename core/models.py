0d11620c13e63b34d7dcf4f1927eba048e996fdc
Midhun <midhunskani@gmail.com>
2024-08-16 20:23:41 +0530

    minor updates

--- core/models.py
+++ core/models.py
@@ -9,6 +9,8 @@ class Hospital(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
     address = models.TextField(max_length=500)
+    phone_number_1 = models.CharField(max_length=15, unique=True)
+    phone_number_2 = models.CharField(max_length=15, unique=True)
 
     def __str__(self):
         return f"{self.id} - {self.name}"
